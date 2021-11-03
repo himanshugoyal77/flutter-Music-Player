7e1ab962543b94865134dd6f9b8a09d20d647caa
himanshu goyal <hiteshgoyal943gmail.com>
2021-11-03 18:28:49 +0530

    day 3

---
+++ lib/pages/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+     initialRoute: "/",
+      routes: {
+        "/": (context) => Homepage(),
+        "/login": (context) => LoginPage()
+      },
+    );
+  }
+}
